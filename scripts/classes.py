f2bbccebf0e006f8148dbb4faded19fb9bfb890e
Johannes Lieberherr <johannes.lieberherr@ttools.ch>
2020-01-09 20:42:46 +0100

    basic classes

--- scripts/classes.py
+++ scripts/classes.py
@@ -6,6 +6,12 @@ class Stop:
         self.easting = easting
         self.northing = northing
 
+class Footpath:
+    def __init__(self, from_stop_id, to_stop_id, walking_time):
+        self.from_stop_id = from_stop_id
+        self.to_stop_id = to_stop_id
+        self.walking_time = walking_time
+
 class Connection:
     def __init__(self, from_stop_id, to_stop_id, dep_time, arr_time):
         self.from_stop_id = from_stop_id
@@ -13,9 +19,5 @@ class Connection:
         self.dep_time = dep_time
         self.arr_time = arr_time
 
-class Footpath:
-    def __init__(self, from_stop_id, to_stop_id, walking_time):
-        self.from_stop_id = from_stop_id
-        self.to_stop_id = to_stop_id
-        self.walking_time = walking_time
+
 
