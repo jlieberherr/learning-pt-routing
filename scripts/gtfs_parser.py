b7e5fb5e684bcfe04b582e33a356879ce047e158
Johannes Lieberherr <johannes.lieberherr@ttools.ch>
2020-01-12 21:04:06 +0100

    parse and test footpaths

--- scripts/gtfs_parser.py
+++ scripts/gtfs_parser.py
@@ -2,22 +2,23 @@ import csv
 from io import TextIOWrapper
 from zipfile import ZipFile
 
-from scripts.classes import Stop
+from scripts.classes import Stop, Footpath
 from scripts.connectionscan_router import ConnectionScanData
 
 
 def get_index_with_default(header, column_name, default_value=None):
     return header.index(column_name) if column_name in header else default_value
+    
 
 def parse_gtfs(path_to_gtfs_zip):
     stops_per_id = {}
+    footpaths_per_from_to_stop_id = {}
     with ZipFile(path_to_gtfs_zip, "r") as zip:
         for name in zip.namelist():
             if name == "stops.txt":
                 with zip.open(name, "r") as gtfs_file:
                     reader = csv.reader(TextIOWrapper(gtfs_file, "utf-8"))
                     header = next(reader)
-                    print(header)
                     id_index = header.index("stop_id") # required
                     code_index = get_index_with_default(header, "stop_code") # optional
                     name_index = get_index_with_default(header, "stop_name") # conditionally required
@@ -32,4 +33,18 @@ def parse_gtfs(path_to_gtfs_zip):
                             float(row[lon_index]) if lon_index else 0.0,
                             float(row[lat_index]) if lat_index else 0.0,
                             )
-    return ConnectionScanData(stops_per_id, {}, {})
+            elif name == "transfers.txt":
+                with zip.open(name, "r") as gtfs_file:
+                    reader = csv.reader(TextIOWrapper(gtfs_file, "utf-8"))
+                    header = next(reader)
+                    from_stop_id_index = header.index("from_stop_id") # required
+                    to_stop_id_index = header.index("to_stop_id") # required
+                    transfer_type_index = header.index("transfer_type") # required
+                    min_transfer_time = header.index("min_transfer_time") # optional
+                    for row in reader:
+                        if row[transfer_type_index] == "2":
+                            from_stop_id = row[from_stop_id_index]
+                            to_stop_id = row[to_stop_id_index]
+                            footpaths_per_from_to_stop_id[(from_stop_id, to_stop_id)] = Footpath(from_stop_id, to_stop_id, int(row[min_transfer_time]))
+
+    return ConnectionScanData(stops_per_id, footpaths_per_from_to_stop_id, {})
--- tests/03_gtfs_parser_test.py
+++ tests/03_gtfs_parser_test.py
@@ -6,8 +6,9 @@ from scripts.gtfs_parser import parse_gtfs
 def test_gtfs_parser():
     path = "tests/resources/gtfsfp20192018-12-05_small.zip"
     cs_data = parse_gtfs(path)
+
+    # stops
     assert 89 == len(cs_data.stops_per_id)
-    
     def check_stop(stop_id, exp_code, exp_name, exp_easting, exp_northing):
         a_stop = cs_data.stops_per_id[stop_id]
         assert stop_id == a_stop.id
@@ -19,4 +20,16 @@ def test_gtfs_parser():
     check_stop("8500218:0:7","", "Olten", 7.90768978414808, 47.3522319182299)
     check_stop("8587654", "", "Glattbrugg, Glatthof", 8.56762812456551, 47.434511142518)
     check_stop("8594553","", "Opfikon, Schwimmbad", 8.57155376235766, 47.4326456250948)
+
+    # footpaths
+    assert 168 == len(cs_data.footpaths_per_from_to_stop_id)
+    def check_footpath(from_stop_id, to_stop_id, exp_walking_time):
+        a_footpath = cs_data.footpaths_per_from_to_stop_id[(from_stop_id, to_stop_id)]
+        assert from_stop_id == a_footpath.from_stop_id
+        assert to_stop_id == a_footpath.to_stop_id
+        assert exp_walking_time == a_footpath.walking_time
+    
+    check_footpath("8500218:0:8", "8500218:0:7", 300)
+    check_footpath("8503000:0:34", "8503000:0:14", 420)
+    check_footpath("8501026:0:3", "8501026:0:1", 120)
 
